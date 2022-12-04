5176bd8bc0536669d17a5b4b75a5dde6fee82e02
Licheng Wang <lichengwang@kkstream.com>
2022-12-04 12:55:42 +0800

    rari usdc-eth pool reentrance hack reproduce

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-ethers');
+require("dotenv").config();
+
+const {ALCHEMY_API_KEY, NODE_BASE_URL} = process.env
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `${NODE_BASE_URL}${ALCHEMY_API_KEY}`,
+        blockNumber:14684813,
+        enable: true
+      }
+    }
+  }
+};
